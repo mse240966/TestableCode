eb5699bd8d26368e1e5308b7971425d93d10bf24
Mark Evans <mse240966@googlemail.com>
2020-01-04 19:58:20 +0000

    More

---
+++ TestableCode.Example.UnitTests/AddHoursToCurrentTimeTestData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TestableCode.Example.UnitTests
+{
+    public class AddHoursToCurrentTimeTestData
+    {
+        public DateTime CurrentDateTime { get; set; }
+        public DateTime ExpectedResult { get; set; }
+        public int Hours { get; set; }
+    }
+}
---
+++ TestableCode.Example.UnitTests/HoursCalculatorGoodTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace TestableCode.Example.UnitTests
+{
+    public class HoursCalculatorGoodTests
+    {
+        public IHoursCalculator _hoursCalculator;
+
+        [TestCaseSource("AddHoursToCurrentTimeTestData")]
+        public void Test_AddHoursToCurrentTime_Returns_Correct_Value(AddHoursToCurrentTimeTestData testCaseData)
+        {
+            _hoursCalculator = new HoursCalculatorGood();
+        }
+
+        public IEnumerable<AddHoursToCurrentTimeTestData> AddHoursToCurrentTimeTestData()
+        {
+            yield return new AddHoursToCurrentTimeTestData() { CurrentDateTime = new DateTime(2020, 01, 02, 08, 30, 10), ExpectedResult = new DateTime(2020, 01, 02, 10, 30, 10), Hours = 2 };
+        }
+
+    }
+}
